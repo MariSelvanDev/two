9eaec009e382b84f456aa35ef6fa7d1ea5bc96e5
MariSelvanDev <ravindrajaddu25@gmail.com>
2017-09-09 15:38:31 +0530

    adding server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express= require("express");
+var app= express();
+
+
+app.get("/",function(req,res){
+res.send("This is my first aws project");
+});
+
+var port = process.env.port || 80
+
+app.listen(port,function(req,res){
+console.log("Port is listening to 3000");
+});

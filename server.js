c426a6ac4c872e7255d9533c1288f9da44d7989a
MariSelvanDev <ravindrajaddu25@gmail.com>
2017-09-10 08:12:16 +0530

    sdd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app= express();
 
 
 app.get("/",function(req,res){
-res.send("This is my first aws project");
+res.send("This is my first fucking project");
 });
 
 var port = process.env.port || 80
